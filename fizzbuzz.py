bdf95c96d4e53b1428df1b3c9b00ee6d3e5deaa8
Kim Jeonghyun <kimjeonghyun.jkim@gmail.com>
2021-03-04 11:46:21 +0900

    refactor: Change all string to lowercase

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,13 @@
 
 def print_(obj):
 	print(obj, end='\t')
-for i in range (1,100+1):
+for i in range (1,300+1):
 	if i%15  == 0:
-		print_('피즈버즈')
+		print_('fizzbuzz')
 	elif i% 3 ==0:
-		print_('FIZZ')
+		print_('fizz')
 	elif i% 5 ==0:
-		print_('BUZZ')
+		print_('buzz')
 	else:
 		print_(i)
 print('') 
