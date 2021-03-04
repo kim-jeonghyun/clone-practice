765e3fe7516cafbef873c5eec2514b2b808830af
Kim Jeonghyun <kimjeonghyun.jkim@gmail.com>
2021-03-04 11:09:03 +0900

    refactor: change print line to Korean

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,11 +3,11 @@ def print_(obj):
 	print(obj, end='\t')
 for i in range (1,100+1):
 	if i%15  == 0:
-		print_('fizzbuzz')
+		print_('피즈버즈')
 	elif i% 3 ==0:
-		print_('fizz')
+		print_('피즈')
 	elif i% 5 ==0:
-		print_('buzz')
+		print_('버즈')
 
 	else:
 		print_(i)
