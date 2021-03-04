e2133fc5dd91c21959495ac59595229ef890c66a
Kim Jeonghyun <kimjeonghyun.jkim@gmail.com>
2021-03-04 11:11:49 +0900

    refactor: change print words to upper case

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,11 +3,11 @@ def print_(obj):
 	print(obj, end='\t')
 for i in range (1,100+1):
 	if i%15  == 0:
-		print_('fizzbuzz')
+		print_('FIZZBUZZ')
 	elif i% 3 ==0:
-		print_('fizz')
+		print_('FIZZ')
 	elif i% 5 ==0:
-		print_('buzz')
+		print_('BUZZ')
 
 	else:
 		print_(i)
