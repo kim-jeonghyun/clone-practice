0b9fd55f944cfb4f95df052366fa3c47923a955a
Kim Jeonghyun <kimjeonghyun.jkim@gmail.com>
2021-03-04 10:26:53 +0900

    feat: add conditional statement to fizzbuzz iteration
    while i goes up to 1 to 100,~
    
    if i is times of 3: print 'fizz'
    if i is times of 5: print 'buzz'
    if i is times of 15: print 'fizzbuzz'
    else, print i

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,11 @@
 for i in range (1,100+1):
-	print(i, end='\t')
-print('')
+	if i%15  == 0:
+		print('fizzbuzz', end='\t' )
+	elif i% 3 ==0:
+		print('fizz', end='\t')
+	elif i% 5 ==0:
+		print('buzz',  end='\t')
+
+	else:
+		print(i, end='\t')
+print('') 
